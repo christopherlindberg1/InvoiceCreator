808b97ff664609839dd0bfacc160b0ba5b7316ed
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-03-21 14:33:48 +0100

    Initial commit

---
+++ App/InvoiceCreator/DataAccess/FilePaths.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    /// <summary>
+    /// Class containing file paths that are used within the app.
+    /// </summary>
+    public class FilePaths
+    {
+        public static string DataStorageRootFolderPath
+        {
+            get
+            {
+                return Path.GetFullPath(
+                    Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\..\DataAccess\Storage\"));
+            }
+        }
+
+        public static string FlightLogsDataFolderPath
+        {
+            get
+            {
+                return Path.GetFullPath(
+                    Path.Combine(DataStorageRootFolderPath, @".\FlightLogs\"));
+            }
+        }
+
+        public static string FlightLogFilePath
+        {
+            get
+            {
+                return Path.GetFullPath(
+                    Path.Combine(FlightLogsDataFolderPath, @".\FlightLog.xml"));
+            }
+        }
+    }
+}
---
+++ App/InvoiceCreator/DataAccess/TextFileUtility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DataAccess
+{
+    public class TextFileUtility
+    {
+        public static List<string> ReadLines(string filePath)
+        {
+            if (String.IsNullOrWhiteSpace(filePath))
+            {
+                throw new ArgumentNullException("filePath", "filePath cannot be null or empty");
+            }
+
+            List<string> lines = new List<string>();
+
+            try
+            {
+                lines = File.ReadAllLines(filePath).ToList<string>();
+
+                return lines;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
