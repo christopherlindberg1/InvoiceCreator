3581f76135b9942d2ff9c70b727ea97ba8ed62a8
christopherlindberg1 <christopher.lindberg1@gmail.com>
2021-03-22 20:15:02 +0100

    Added empty DataGrid

---
+++ App/InvoiceCreator/DataAccess/Invoice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class Invoice
+    {
+        private readonly string[] _items;
+        
+        public string[] Items
+        {
+            get => _items;
+
+            init => _items = value ??
+                throw new ArgumentNullException("Items", "Items cannot be null");
+        }
+
+        public Invoice(string[] items)
+        {
+            Items = items;
+        }
+    }
+}
--- App/InvoiceCreator/InvoiceCreatorWPF/MainWindow.xaml.cs
+++ App/InvoiceCreator/InvoiceCreatorWPF/MainWindow.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using DataAccess;
 using Microsoft.Win32;
 
 namespace InvoiceCreatorWPF
@@ -23,30 +22,74 @@ namespace InvoiceCreatorWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Invoice _invoice;
+
+
+
+
+
+
+        // ======================== Properties ======================== //
+
+        public Invoice Invoice
+        {
+            get => _invoice;
+
+            set => _invoice = value;
+        }
+
+
+
+
+
+
+        // ======================== Methods ======================== //
+
         public MainWindow()
         {
             InitializeComponent();
-
-            //OpenFileDialog openFileDialog = new OpenFileDialog();
-            //openFileDialog.InitialDirectory = FilePaths.InvoicesDataFolderPath;
-            //openFileDialog.Title = "Choose invoice to open";
-            //openFileDialog.Filter = "Text Files | *.txt";
-            
-            //bool? result = openFileDialog.ShowDialog();
-
-            //if (result == null || result == false)
-            //{
-            //    return;
-            //}
-
-            //string filePath = System.IO.Path.GetFullPath(
-            //        System.IO.Path.Combine(FilePaths.InvoicesDataFolderPath, @".\InvoiceDemo1.txt"));
-
-            //List<string> lines = TextFileUtility.ReadAllLines(openFileDialog.FileName);
-
-            //MessageBox.Show(lines.Count.ToString());
+        }
 
 
+
+
+
+        // ======================== Event handler methods ======================== //
+
+        private void LoadInvoice_EventHandler()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = FilePaths.InvoicesDataFolderPath;
+            openFileDialog.Title = "Choose invoice to open";
+            openFileDialog.Filter = "Text Files | *.txt";
+
+            bool? result = openFileDialog.ShowDialog();
+
+            if (result == null || result == false)
+            {
+                return;
+            }
+
+            try
+            {
+                string[] invoiceItems = TextFileUtility.GetInvoiceItems(openFileDialog.FileName);
+                Invoice = new Invoice(invoiceItems);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+
+
+
+
+        // ======================== Events ======================== //
+
+        private void btnLoadInvoice_Click(object sender, RoutedEventArgs e)
+        {
+            LoadInvoice_EventHandler();
         }
     }
 }
